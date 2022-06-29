823e72642057dfed25b096c1bfeac21697d6ea43
wangjiaxuan666 <poormouse@126.com>
2022-06-29 23:47:17 +0800

    remove files

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = [ ]
 
@@ -36,7 +31,6 @@ setup(
     },
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='cellname',
     name='cellname',
